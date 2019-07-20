42a6eb52fdbfb9169df3fc8d8d3e8c529607d6a4
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-07-20 16:16:31 -0400

    Longhand notation and explicit options.

--- mnist.cmd
+++ mnist.cmd
@@ -1,9 +1,12 @@
 #!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:05:00
+#SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
-#SBATCH -t 00:05:00
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
-#SBATCH --mail-user=yournetid @princeton.edu
+#SBATCH --mail-user=$USER@princeton.edu
 
 module load anaconda3
 source activate tf-gpu
