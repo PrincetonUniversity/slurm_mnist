82544682e5c97937fc82dfe28c5e201041b78b01
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-07-20 16:53:12 -0400

    Cannot automate username insertion.

--- mnist.cmd
+++ mnist.cmd
@@ -4,10 +4,12 @@
 #SBATCH --time=00:05:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
+#SBATCH --job-name="tensorflow_tutorial"
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
-#SBATCH --mail-user=$USER@princeton.edu
+#SBATCH --mail-user=YourNetID@princeton.edu
 
 module load anaconda3
 source activate tf-gpu
-python mnist_classify.py
+
+srun python mnist_classify.py
