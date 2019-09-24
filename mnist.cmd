a557024d33749ab13bbba3cf207c8c83ef320e98
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-09-23 22:07:39 -0400

    Added comments to options

--- mnist.cmd
+++ mnist.cmd
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=00:05:00
-#SBATCH --mem-per-cpu=4G
-#SBATCH --gres=gpu:1
-#SBATCH --job-name="tensorflow_tutorial"
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
+#SBATCH --job-name=tf-test       # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multithread tasks
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --gres=gpu:1             # number of gpus per node
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=YourNetID@princeton.edu
 
 module load anaconda3
