a7e563074860bbd81ae04a6d93afec83141fb9f7
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-09-30 16:36:39 -0400

    Reformatted

--- mnist.cmd
+++ mnist.cmd
@@ -2,15 +2,15 @@
 #SBATCH --job-name=tf-test       # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multithread tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core
-#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --gres=gpu:1             # number of gpus per node
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=YourNetID@princeton.edu
 
 module load anaconda3
-source activate tf-gpu
+conda activate tf-gpu
 
 srun python mnist_classify.py
