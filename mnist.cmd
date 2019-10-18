5fbff8f059fc4c2166f5e43cae3bd0decfb21ffa
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-10-18 00:11:11 -0400

    Update mnist.cmd

--- mnist.cmd
+++ mnist.cmd
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --mem=4G                 # total memory per node
 #SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
