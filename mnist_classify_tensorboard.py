11e0ce4dc2a519f57f0a7d7092abd7fb700c751a
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-28 16:21:29 -0400

    Create mnist_classify_tensorboard.py

---
+++ mnist_classify_tensorboard.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+import datetime
+log_dir = "./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+model.fit(x_train, y_train, epochs=10, callbacks=[tensorboard_callback])
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+print('\nTest accuracy:', test_acc)
