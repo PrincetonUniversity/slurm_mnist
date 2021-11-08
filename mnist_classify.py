ab639aa875664f1e9c4912cbfb18f5fa11e790d5
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-11-08 16:34:36 -0500

    Update mnist_classify.py

--- mnist_classify.py
+++ mnist_classify.py
@@ -1,14 +1,16 @@
+# see https://www.tensorflow.org/tutorials/keras/classification
+
 import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')])
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
